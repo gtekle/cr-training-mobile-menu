d8959d2aff8cf798c33416d63085609bbe97d3fa
tYohannes <teklegy@gmail.com>
2022-04-14 10:55:34 +0300

    Fix linter errors

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let mobileNavMenu = document.querySelector('.mobile-nav');
 // let button1 = document.querySelector('.btn-menu-close');
 // let button2 = document.querySelector('.menu-bars');
 
-console.log('mobile nav....')
+console.log('mobile nav....');
 
 // button1.addEventListener('click', (event) => {
 //   event.preventDefault();
@@ -11,9 +11,8 @@ console.log('mobile nav....')
 //   body.style.overflow = '';
 // });
 
-
-function openMenu(){
-  console.log('menu bars clicked....')
+function openMenu() {
+  console.log('menu bars clicked....');
   mobileNavMenu = document.querySelector('.mobile-nav');
   body = document.querySelector('body');
   mobileNavMenu.style.display = 'block';
@@ -21,14 +20,13 @@ function openMenu(){
   console.log(mobileNavMenu.style.display);
 }
 
-
 // button2.addEventListener('click', (event) => {
 //   event.preventDefault();
 //   mobileNavMenu.style.display = 'none';
 //   body.style.overflow = '';
 // });
 
-function closeMenu(){
+function closeMenu() {
   console.log('clicked close button');
   mobileNavMenu = document.querySelector('.mobile-nav');
   body = document.querySelector('body');
@@ -36,3 +34,5 @@ function closeMenu(){
   body.style.overflow = '';
   console.log(mobileNavMenu.style.display);
 }
+
+console.log(openMenu(), closeMenu());
