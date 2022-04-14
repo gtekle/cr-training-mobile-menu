a088b5c19a27c3b13f11514514fb9cf543cc9ff1
tYohannes <teklegy@gmail.com>
2022-04-14 22:45:36 +0300

    Add feature to make mobile menu disappears and displays correct part of page when nav link is clicked

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
 let body = document.querySelector('body');
 let mobileNavMenu = document.querySelector('.mobile-nav');
 
-console.log('mobile nav....');
-
 function openMenu() {
-  console.log('menu bars clicked....');
   mobileNavMenu = document.querySelector('.mobile-nav');
   body = document.querySelector('body');
   mobileNavMenu.style.display = 'block';
   body.style.overflow = 'hidden';
-  console.log(mobileNavMenu.style.display);
 }
 
 function closeMenu() {
-  console.log('clicked close button');
   mobileNavMenu = document.querySelector('.mobile-nav');
   body = document.querySelector('body');
   mobileNavMenu.style.display = 'none';
   body.style.overflow = '';
   console.log(mobileNavMenu.style.display);
 }
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((navLink) => {
+  body = document.querySelector('body');
+  navLink.addEventListener('click', () => {
+    mobileNavMenu.style.display = 'none';
+    body.style.overflow = '';
+  });
+});
 
 console.log(openMenu(), closeMenu());
