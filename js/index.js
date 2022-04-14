7bf6e4465b04af8570be65c789eb633a7553d338
tYohannes <teklegy@gmail.com>
2022-04-14 10:46:04 +0300

    Add javascript click events to open and close menu buttons

---
+++ js/index.js
@@ -0,0 +1,38 @@
+let body = document.querySelector('body');
+let mobileNavMenu = document.querySelector('.mobile-nav');
+// let button1 = document.querySelector('.btn-menu-close');
+// let button2 = document.querySelector('.menu-bars');
+
+console.log('mobile nav....')
+
+// button1.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   mobileNavMenu.style.display = 'none';
+//   body.style.overflow = '';
+// });
+
+
+function openMenu(){
+  console.log('menu bars clicked....')
+  mobileNavMenu = document.querySelector('.mobile-nav');
+  body = document.querySelector('body');
+  mobileNavMenu.style.display = 'block';
+  body.style.overflow = 'hidden';
+  console.log(mobileNavMenu.style.display);
+}
+
+
+// button2.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   mobileNavMenu.style.display = 'none';
+//   body.style.overflow = '';
+// });
+
+function closeMenu(){
+  console.log('clicked close button');
+  mobileNavMenu = document.querySelector('.mobile-nav');
+  body = document.querySelector('body');
+  mobileNavMenu.style.display = 'none';
+  body.style.overflow = '';
+  console.log(mobileNavMenu.style.display);
+}
